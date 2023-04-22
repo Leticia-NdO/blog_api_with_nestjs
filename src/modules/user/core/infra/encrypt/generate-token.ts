064ec6056c6796504064c34304b55cfc1ec43635
Leticia-NdO <3972656@alunouninter.com>
2023-04-22 18:17:27 -0300

    feat: add JWT token generator

---
+++ src/modules/user/core/infra/encrypt/generate-token.ts
@@ -0,0 +1,13 @@
+import { sign } from "jsonwebtoken";
+import { UserEntity } from "../../domain/user.entity";
+
+export const generateToken = (userEntity: UserEntity): string => {
+  return sign(
+    {
+      id: userEntity.id,
+      username: userEntity.username,
+      email: userEntity.email,
+    },
+    process.env.ENCRYPTING_SECRET
+  );
+}
