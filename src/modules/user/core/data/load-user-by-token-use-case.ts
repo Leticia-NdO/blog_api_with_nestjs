d3735bacb9f693cc3dc44ba965649bfc75b3cc95
Leticia-NdO <3972656@alunouninter.com>
2023-05-04 17:48:14 -0300

    fix: change return type in load user by token use case from UserResponse to UserEntity

--- src/modules/user/core/data/load-user-by-token-use-case.ts
+++ src/modules/user/core/data/load-user-by-token-use-case.ts
@@ -1,19 +1,17 @@
 import { verify } from 'jsonwebtoken';
-import { UserResponse } from '../../types/user-response.interface';
 import { UserType } from '../../types/user.type';
 import { FindUserByIdRepositoryInterface } from '../domain/repository/find-user-by-id-repository-interface';
-import { buildUserResponse } from './helpers/user-response-helper';
+import { UserEntity } from '../domain/user.entity';
 
 export class LoadUserByTokenUseCase {
   constructor(
     private readonly findUserByIdRepository: FindUserByIdRepositoryInterface
   ) {}
 
-  async find (token: string): Promise<UserResponse> {
+  async find (token: string): Promise<UserEntity> {
     const userAccount = verify(token, process.env.JWT_SECRET) as UserType;
     if (userAccount) {
-      const userByToken = await this.findUserByIdRepository.find(userAccount.id)
-      return buildUserResponse(userByToken)
+      return await this.findUserByIdRepository.find(userAccount.id)
     }
   }
 }
