e1be694f637d0b48069f7826915307937a654662
Leticia-NdO <3972656@alunouninter.com>
2023-05-05 11:38:28 -0300

    refactor: add find one follow repository interface and unfollow profile repository interface to unfollow profile use-case and remove typeorm dependencies

--- src/modules/profile/core/data/unfollow-profile-use-case.ts
+++ src/modules/profile/core/data/unfollow-profile-use-case.ts
@@ -1,23 +1,19 @@
 import { UserEntity } from '@app/modules/user/core/domain/user.entity'
-import { Repository } from 'typeorm'
 import { Profile } from '../../types/profile.interface'
-import { FollowEntity } from '../domain/follow.entity'
+import { FindOneFollowRepositoryInterface } from '../domain/repository/find-one-follow-repository-interface'
+import { UnfollowProfileRepositoryInterface } from '../domain/repository/unfollow-profile-repository-interface'
 
 export class UnfollowProfileUseCase {
   constructor (
-    private readonly followRepository: Repository<FollowEntity>
+    private readonly findOneFollowRepository: FindOneFollowRepositoryInterface,
+    private readonly unfollowRepository: UnfollowProfileRepositoryInterface
   ) {}
 
   async unfollowProfile (profileToBeUnfollowed: UserEntity, userId: number): Promise<Profile> {
-    const follow = await this.followRepository.findOne({
-      where: {
-        followerId: userId,
-        followingId: profileToBeUnfollowed.id
-      }
-    })
+    const follow = await this.findOneFollowRepository.find(userId, profileToBeUnfollowed.id)
 
     if (follow) {
-      await this.followRepository.delete(follow.id)
+      await this.unfollowRepository.unfollow(userId, profileToBeUnfollowed.id)
     }
 
     return {
