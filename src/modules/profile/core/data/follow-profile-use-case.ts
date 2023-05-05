4d0bfc168dc38df1af81bf78eaf879eb865eb9c4
Leticia-NdO <3972656@alunouninter.com>
2023-05-05 10:49:14 -0300

    refactor: add find one follow repository interface and follow profile repository interface to follow profile use-case and remove typeorm Repository

--- src/modules/profile/core/data/follow-profile-use-case.ts
+++ src/modules/profile/core/data/follow-profile-use-case.ts
@@ -1,26 +1,19 @@
 import { UserEntity } from '@app/modules/user/core/domain/user.entity'
-import { Repository } from 'typeorm'
 import { Profile } from '../../types/profile.interface'
-import { FollowEntity } from '../domain/follow.entity'
+import { FindOneFollowRepositoryInterface } from '../domain/repository/find-one-follow-repository-interface'
+import { FollowProfileRepositoryInterface } from '../domain/repository/follow-profile-repository-interface'
 
 export class FollowProfileUseCase {
   constructor (
-    private readonly followRepository: Repository<FollowEntity>
+    private readonly findOneFollowRepository: FindOneFollowRepositoryInterface,
+    private readonly followProfileRepository: FollowProfileRepositoryInterface
   ) {}
 
   async followProfile (profileToBeFollowed: UserEntity, userId: number): Promise<Profile> {
-    const follow = await this.followRepository.findOne({
-      where: {
-        followerId: userId,
-        followingId: profileToBeFollowed.id
-      }
-    })
+    const follow = await this.findOneFollowRepository.find(userId, profileToBeFollowed.id)
 
     if (!follow) {
-      const followToCreate = new FollowEntity()
-      followToCreate.followerId = userId
-      followToCreate.followingId = profileToBeFollowed.id
-      await this.followRepository.save(followToCreate)
+      await this.followProfileRepository.follow(userId, profileToBeFollowed.id)
     }
 
     return {
