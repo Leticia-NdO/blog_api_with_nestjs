ead258e033bb84cba3f2445af353b102e30dcc36
Leticia-NdO <3972656@alunouninter.com>
2023-05-09 19:19:28 -0300

    fix: adjust auth-middleware to use user.service instead of load user by token use case

--- src/modules/user/middleware/auth-middleware.ts
+++ src/modules/user/middleware/auth-middleware.ts
@@ -1,11 +1,12 @@
 import { Injectable, NestMiddleware } from '@nestjs/common'
 import { NextFunction, Response } from 'express'
 import { ExpressRequest } from '@app/types/express-request.interface'
-import { LoadUserByTokenUseCase } from '../core/data/load-user-by-token-use-case'
+import { UserService } from '../user.service'
+
 // without next we won't go inside our controller
 @Injectable()
 export class AuthMiddleWare implements NestMiddleware {
-  constructor (private readonly loadUserByTokenUseCase: LoadUserByTokenUseCase) {}
+  constructor (private readonly userService: UserService) {}
 
   async use (req: ExpressRequest, _: Response, next: NextFunction): Promise<void> {
     if (!req.headers.authorization) {
@@ -14,8 +15,12 @@ export class AuthMiddleWare implements NestMiddleware {
     } else {
       const token = req.headers.authorization.split(' ')[1]
 
+      if (!token) {
+        req.user = null
+        next()
+      }
       try {
-        const user = await this.loadUserByTokenUseCase.find(token)
+        const user = await this.userService.loadUserByToken(token)
         if (!user) {
           req.user = null
           next()
